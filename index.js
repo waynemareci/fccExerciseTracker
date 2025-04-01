a0347010f0d87759115be7a577bed19a2d81fdb9
waynemareci <wayne@mareci.com>
2025-04-01 01:17:34 +0000

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const res = require('express/lib/response');
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,9 +13,19 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next()
+})
 
+app.post('/api/users', (req, res) => {
+  function isValidURL (string) {
+    const pattern = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?$/
+    return pattern.test(string)
+  }
+  console.log('body: ' + req.body)
 
-
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
